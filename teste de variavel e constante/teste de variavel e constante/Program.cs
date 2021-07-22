c2ab1fd92e599d8258e7dcc16988f29dc747a036
Princess kelly <01kellyprincess@gmail.com>
2021-07-22 17:10:24 -0300

    primeiro comit c#

---
+++ teste de variavel e constante/teste de variavel e constante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace teste_de_variavel_e_constante
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+        {
+            const string constante = "uma constante que não muda";
+
+            if(argumentos.Length == 0)
+            {
+                Console.WriteLine("Favor informar um número inteiro");
+                return;
+            }
+
+            int inteiro;
+            bool test = int.TryParse(argumentos[0], out inteiro);
+
+            if(test == false)
+            {
+                Console.WriteLine("O argumento informado não é um número inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+        }
+    }
+}
