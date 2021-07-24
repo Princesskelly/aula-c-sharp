2402bee44d6f6b1fcef48a0d196cc00d097b2423
Princess kelly <01kellyprincess@gmail.com>
2021-07-24 16:45:17 -0300

    Aulas 3 e 4

---
+++ listadeconvidados/listadeconvidados/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace listadeconvidados
+{
+    class Program
+    {
+        private static string[] ListaDeConvidados =
+        {
+            "Daniel",
+            "Evandro",
+            "Matheus",
+            "Ale",
+            "Teresa",
+            "Gabriel"
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do convidado:");
+            var nome = Console.ReadLine();
+
+            if(nome == null || string.IsNullOrEmpty(nome))
+            {
+                Console.WriteLine("Nome não informado para seguir o programa");
+                return;
+            }
+
+            Console.WriteLine("Informe a idade do convidado:");
+            var idadeString = Console.ReadLine();
+            int idade;
+
+            bool idadeInformada = int.TryParse(idadeString, out idade);
+
+            if(idadeInformada == false)
+            {
+                Console.WriteLine("Idade não informada para seguir o programa");
+                return;
+            }
+
+            bool estaConvidado ;
+            switch (nome)
+            {
+                case "Daniel":
+                    estaConvidado = true;
+                    break;
+                case "Evandro":
+                    estaConvidado = true;
+                    break;
+                case "Matheus":
+                    estaConvidado = true;
+                    break;
+                case "Ale":
+                    estaConvidado = true;
+                    break;
+                case "Teresa":
+                    estaConvidado = true;
+                    break;
+                case "Gabriel":
+                    estaConvidado = true;
+                    break;
+                case "Manoel":
+                    estaConvidado = true;
+                    break;
+                default:
+                    estaConvidado = false;
+                    break;
+            }
+
+            if(estaConvidado == true && idade > 18)
+            {
+                Console.WriteLine("Parabéns, seja bem vindo à festa!");
+            }else if(estaConvidado == false)
+            {
+                Console.WriteLine("Você não está convidado, não vai poder entrar");
+            }
+            else
+            {
+                Console.WriteLine("Você não tem 18 anos para entrar na festa");
+            }
+        }
+    }
+}
